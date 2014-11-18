ab66b339a7d76b2d837a1bfe0a0d4b7ccbb6c9d3
Patrick Vos <Pat_Vos-@hotmail.com>
2014-11-18 10:57:17 +0100

    - testing commit

--- Qars/Qars/Program.cs
+++ Qars/Qars/Program.cs
@@ -14,6 +14,7 @@ namespace Qars {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //test comment
         }
     }
 }
