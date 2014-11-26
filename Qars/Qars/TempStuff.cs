0fcc27cbaa7b538288ad9f8512b822dbca4094c3
Kavhin Sgeaninhg <Kjscheening@gmail.com>
2014-11-26 13:26:50 +0100

    Grafische Alpha van Qars Project

---
+++ Qars/Qars/TempStuff.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qars
+{
+    static class TempStuff
+    {
+        public static bool first = true;
+        
+    }
+}
--- qarstest/qarstest/Form1.cs
+++ qarstest/qarstest/Form1.cs
@@ -65,5 +65,10 @@ namespace qarstest {
         private void label14_Click(object sender, EventArgs e) {
 
         }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
