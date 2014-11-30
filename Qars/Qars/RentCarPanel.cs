e8f092d93ae647e6261d754225952dbb6fc6a3b1
Niels Boerkamp <niels95b@gmail.com>
2014-11-30 16:17:32 +0100

    manual merge Factuurscherm with Factuurscherm2

---
+++ Qars/Qars/RentCarPanel.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Qars
+{
+    public partial class RentCarPanel : UserControl
+    {
+        private String currentSelectedDateBox;
+        public RentCarPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void monthCalendar_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            string day = monthCalendar.SelectionStart.Day.ToString();
+            string month = monthCalendar.SelectionStart.Month.ToString();
+
+            if (day.Length == 1)
+            {
+                day = "0" + day;
+            }
+
+            if (month.Length == 1)
+            {
+                month = "0" + month;
+            }
+
+            string date = day + month + monthCalendar.SelectionStart.Year.ToString();
+
+            if (startdateTextbox.Name == currentSelectedDateBox)
+            {
+                startdateTextbox.Text = date;
+            }
+            else if (enddateTextbox.Name == currentSelectedDateBox)
+            {
+                enddateTextbox.Text = date;
+            }
+
+
+        }
+
+        private void openCalender(object sender, EventArgs e)
+        {
+            monthCalendar.MinDate = DateTime.Today;
+            monthCalendar.Show();
+            MaskedTextBox b = (MaskedTextBox)sender;
+            currentSelectedDateBox = b.Name;
+
+        }
+
+        private void closeCalender(object sender, EventArgs e)
+        {
+            monthCalendar.Hide();
+
+            if (currentSelectedDateBox == startdateTextbox.Name)
+            {
+                monthCalendar.MinDate = monthCalendar.SelectionStart.Date;
+            }
+        }
+
+        private void rentCarClick(object sender, EventArgs e)
+        {
+            Util.Mail mail = new Util.Mail();
+            //some variables that needs to be checked
+            try
+            {
+                int streetnumber = Convert.ToInt32(streetnumberTextbox.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Uw huisnummer klopt niet");
+            }
+
+            try
+            { //check if emailaddress is valid
+                MailAddress email = new MailAddress(emailTextbox.Text);
+                mail.addTo(emailTextbox.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Geen geldig emailadres");
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("U heeft geen emailadres ingevuld");
+            }
+
+            mail.addSubject("Aanvraag van Audi A4");
+            mail.addBody(buildEmailBody());
+
+            mail.sendEmail();
+            MessageBox.Show("Er is");
+        }
+
+        private string buildEmailBody()
+        {
+            StringBuilder builder = new StringBuilder();
+            //This part can be edited by the admin
+            builder.AppendLine(String.Format("Beste meneer/mevrouw {0}", lastnameTextbox.Text));
+            builder.AppendLine("");
+            builder.AppendLine("Bedank voor uw bestelling bij Qars. Wij gaan proberen om uw verzoek zo snel");
+            builder.AppendLine("mogelijk te verwerken. Zodra uw verzoek is goed gekeurd ontvangt u een bestiging");
+            builder.AppendLine("");
+
+            builder.AppendLine(String.Format("Voornaam:\t{0}", firstnameTextbox.Text));
+            builder.AppendLine(String.Format("Achternaam:\t{0}", lastnameTextbox.Text));
+            builder.AppendLine(String.Format("Adres:\t\t{0} {1}{2}", streetnameTextbox.Text, streetnumberTextbox.Text , streetnumbersuffixTextbox.Text));
+            builder.AppendLine(String.Format("Woonplaats:\t{0}", cityTextbox.Text));
+            builder.AppendLine(String.Format("Postcode:\t{0}", postalcodeTextbox.Text));
+            builder.AppendLine(String.Format("Email:\t\t{0}", emailTextbox.Text));
+            builder.AppendLine(String.Format("Telefoon:\t{0}", phonenumberTextbox.Text));
+            builder.Append("\n");
+            builder.AppendLine(String.Format("Begin datum:\t{0}", startdateTextbox.Text));
+            builder.AppendLine(String.Format("Eind datum:\t{0}", enddateTextbox.Text));
+            builder.Append("\n");
+            builder.AppendLine("Opmerking:");
+            builder.AppendLine(commentTextbox.Text);
+
+            //this part can be edited by the admin
+            builder.Append("\n\n");
+            builder.AppendLine("Met vriendelijke groeten,");
+            builder.AppendLine("");
+            builder.AppendLine("Qars");
+                
+            return builder.ToString();
+        }
+
+    }
+}
