5961369b6cc63d0e07df0f3daa66a73d6e13f7c8
Delano <delanoderuiter@hotmail.com>
2014-12-10 10:19:14 +0100

    comparepanel picture size update and text size update

--- Qars/Qars/ComparePanel.cs
+++ Qars/Qars/ComparePanel.cs
@@ -11,9 +11,9 @@ namespace Qars
     class ComparePanel : Panel
     {
         public List<Car> cars { get; set; }
-        public string[] compareItems = { "rentalprice", "brand", "category", "modelyear", "horsepower", "doors", "seats", "Fuelusage", "motor" };
+        public string[] compareItems = { "rentalprice", "brand", "model", "category", "modelyear", "horsepower", "doors", "seats", "Fuelusage", "motor" };
         //items in code are given in English. In the application the translation is shown.
-        public string[] compareItemsTranslation = { "Huurprijs", "Merk", "Categorie", "Bouwjaar", "Vermogen", "Deuren", "Stoelen", "Verbruik", "Motor" };
+        public string[] compareItemsTranslation = { "Huurprijs", "Merk", "model", "Categorie", "Bouwjaar", "Vermogen", "Deuren", "Stoelen", "Verbruik", "Motor" };
         public List<string> checkHighest = new List<String>{ "horsepower","seats" };
         public List<string> checkLowest = new List<String> { "rentalprice","Fuelusage"};
         public List<Label> allLabels { get; set; }
@@ -29,8 +29,8 @@ namespace Qars
         public ComparePanel(List<Car> list)
         {
             allLabels = new List<Label>();
-            this.Font = new Font("Calibri", 10);
-            sideMargin = 75;
+            this.Font = new Font("Calibri", 14);
+            sideMargin = 100;
             topMargin = 27;
             Height = 570;
             Width = 1045;
@@ -38,7 +38,7 @@ namespace Qars
             pictureHeight = 200;
             pictureMargin = 1;
             labelHeight = 30;
-            this.BackColor = Color.FromArgb(97, 97, 97);
+            this.BackColor = Color.FromArgb(240, 240, 240);
             cars = list;
 
             this.Top = 70;
@@ -63,7 +63,7 @@ namespace Qars
                 Label tempLabel = new Label();
                 tempLabel.Text = compareItemsTranslation[i];
                 tempLabel.Top = topMargin + pictureHeight + labelHeight * i;
-                tempLabel.Width = 75;
+                tempLabel.Width = 100;
                 tempLabel.Height = labelHeight;
                 tempLabel.TextAlign = ContentAlignment.MiddleLeft;
                 tempLabel.BackColor = Color.Transparent;
@@ -82,7 +82,7 @@ namespace Qars
                     pictures[i].Width = columnWidth - pictureMargin * 2;
                     pictures[i].Top = topMargin;
                     pictures[i].Height = pictureHeight;
-                    pictures[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                    pictures[i].SizeMode = PictureBoxSizeMode.Zoom;
                     pictures[i].BorderStyle = BorderStyle.FixedSingle;
                     this.Controls.Add(pictures[i]);
                 }
@@ -117,7 +117,7 @@ namespace Qars
             base.OnPaint(e);
             graphics = e.Graphics;
             Pen pen = new Pen(Color.Black);
-            SolidBrush brush = new SolidBrush(Color.FromArgb(224, 224, 224));
+            SolidBrush brush = new SolidBrush(Color.FromArgb(97, 97, 97));
 
             //rectangle for the items in the list
             for (int i = 0; i < compareItems.Length; i+=2)
@@ -149,7 +149,20 @@ namespace Qars
             double maxValue = compare.Max();
             if (maxValue == Convert.ToDouble(GetPropValue(car,item))){
                 label.Text = label.Text + " \u221A";
-                label.Font = new Font("Calibri", 10, FontStyle.Bold);
+                label.Font = new Font("Calibri", 14, FontStyle.Bold);
+                
+                //big check
+                //Label tempLabel = new Label();
+                //tempLabel.Text = "\u221A";
+                //tempLabel.Font = new Font("Calibri", 20, FontStyle.Bold);
+                //tempLabel.Left = label.Left + label.Width;
+                //tempLabel.Top = label.Top;
+                //tempLabel.Height = labelHeight;
+                //tempLabel.Width = columnWidth;
+                //tempLabel.TextAlign = ContentAlignment.MiddleCenter;
+                //tempLabel.BackColor = Color.Transparent;
+                //this.Controls.Add(tempLabel);
+                //allLabels.Add(tempLabel);
             }
         }
 
@@ -162,7 +175,7 @@ namespace Qars
             double maxValue = compare.Min();
             if (maxValue == Convert.ToDouble(GetPropValue(car, item))){
                 label.Text = label.Text + " \u221A";
-                label.Font = new Font("Calibri", 10, FontStyle.Bold);
+                label.Font = new Font("Calibri", 14, FontStyle.Bold);
             }
         }
 
