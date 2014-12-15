b61b5a45849d8e2787921f82299e5f15b5849318
Stefan Dinkelberg <stefan.dinkelberg@gmail.com>
2014-12-15 11:21:51 +0100

    - added login button

--- Qars/Qars Admin/signInForm.Designer.cs
+++ Qars/Qars Admin/signInForm.Designer.cs
@@ -32,50 +32,67 @@
             this.passwordLabel = new System.Windows.Forms.Label();
             this.usernameTextBox = new System.Windows.Forms.TextBox();
             this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.SignInButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // usernameLabel
             // 
             this.usernameLabel.AutoSize = true;
-            this.usernameLabel.Location = new System.Drawing.Point(12, 37);
+            this.usernameLabel.Location = new System.Drawing.Point(8, 24);
+            this.usernameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.usernameLabel.Name = "usernameLabel";
-            this.usernameLabel.Size = new System.Drawing.Size(127, 20);
+            this.usernameLabel.Size = new System.Drawing.Size(84, 13);
             this.usernameLabel.TabIndex = 0;
             this.usernameLabel.Text = "Gebruikersnaam";
             // 
             // passwordLabel
             // 
             this.passwordLabel.AutoSize = true;
-            this.passwordLabel.Location = new System.Drawing.Point(12, 75);
+            this.passwordLabel.Location = new System.Drawing.Point(8, 49);
+            this.passwordLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.passwordLabel.Name = "passwordLabel";
-            this.passwordLabel.Size = new System.Drawing.Size(98, 20);
+            this.passwordLabel.Size = new System.Drawing.Size(68, 13);
             this.passwordLabel.TabIndex = 1;
             this.passwordLabel.Text = "Wachtwoord";
             // 
             // usernameTextBox
             // 
-            this.usernameTextBox.Location = new System.Drawing.Point(145, 37);
+            this.usernameTextBox.Location = new System.Drawing.Point(97, 24);
+            this.usernameTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.usernameTextBox.Name = "usernameTextBox";
-            this.usernameTextBox.Size = new System.Drawing.Size(253, 26);
+            this.usernameTextBox.Size = new System.Drawing.Size(170, 20);
             this.usernameTextBox.TabIndex = 2;
             // 
             // passwordTextBox
             // 
-            this.passwordTextBox.Location = new System.Drawing.Point(145, 69);
+            this.passwordTextBox.Location = new System.Drawing.Point(97, 45);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.passwordTextBox.Name = "passwordTextBox";
             this.passwordTextBox.PasswordChar = '•';
-            this.passwordTextBox.Size = new System.Drawing.Size(253, 26);
+            this.passwordTextBox.Size = new System.Drawing.Size(170, 20);
             this.passwordTextBox.TabIndex = 3;
+            // 
+            // SignInButton
+            // 
+            this.SignInButton.Location = new System.Drawing.Point(195, 74);
+            this.SignInButton.Name = "SignInButton";
+            this.SignInButton.Size = new System.Drawing.Size(75, 23);
+            this.SignInButton.TabIndex = 4;
+            this.SignInButton.Text = "Log in";
+            this.SignInButton.UseVisualStyleBackColor = true;
+            this.SignInButton.Click += new System.EventHandler(this.SignInButton_Click);
             // 
             // signInForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(423, 167);
+            this.ClientSize = new System.Drawing.Size(282, 109);
+            this.Controls.Add(this.SignInButton);
             this.Controls.Add(this.passwordTextBox);
             this.Controls.Add(this.usernameTextBox);
             this.Controls.Add(this.passwordLabel);
             this.Controls.Add(this.usernameLabel);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "signInForm";
@@ -93,6 +110,7 @@
         private System.Windows.Forms.Label passwordLabel;
         private System.Windows.Forms.TextBox usernameTextBox;
         private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button SignInButton;
     }
 }
 
--- Qars/Qars Admin/signInForm.cs
+++ Qars/Qars Admin/signInForm.cs
@@ -16,5 +16,9 @@ namespace Qars_Admin
         {
             InitializeComponent();
         }
+
+        private void SignInButton_Click(object sender, EventArgs e) {
+
+        }
     }
 }
