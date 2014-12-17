4360063abef05c534ef31cbaf3e8ce21fe7dc51d
Niels Boerkamp <niels95b@gmail.com>
2014-12-17 13:54:46 +0100

    add Buttons TEST

--- Qars/Qars Admin/AdminForm.Designer.cs
+++ Qars/Qars Admin/AdminForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.categoriesLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -40,21 +41,34 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
             // 
+            // categoriesLabel
+            // 
+            this.categoriesLabel.AutoSize = true;
+            this.categoriesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.categoriesLabel.Location = new System.Drawing.Point(12, 12);
+            this.categoriesLabel.Name = "categoriesLabel";
+            this.categoriesLabel.Size = new System.Drawing.Size(183, 37);
+            this.categoriesLabel.TabIndex = 1;
+            this.categoriesLabel.Text = "Categoriën";
+            // 
             // AdminForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1520, 869);
+            this.Controls.Add(this.categoriesLabel);
             this.Controls.Add(this.groupBox1);
             this.Name = "AdminForm";
             this.ShowIcon = false;
             this.Text = "Qars Administratie";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label categoriesLabel;
     }
 }
--- Qars/Qars Admin/AdminForm.cs
+++ Qars/Qars Admin/AdminForm.cs
@@ -15,11 +15,42 @@ namespace Qars_Admin
     {
 
         private User currentUser;
+        private List<Button> categories = new List<Button>();
+        private int yCoordinate = 50;
 
         public AdminForm(User currentUser)
         {
             this.currentUser = currentUser;
             InitializeComponent();
+            addCategories();
+        }
+
+        private void addCategories()
+        {
+            categories.Add(this.createButton("Autos beheren", "autoBeheren", 1));
+            categories.Add(this.createButton("Reserveringen beheren", "reserveringBeheren", 3));
+            categories.Add(this.createButton("Vestigingen beheren", "vestigignBeheren", 5));
+
+            foreach (Button panel in categories)
+            {
+                Controls.Add(panel);
+            }
+
+        }
+
+        private Button createButton(string buttonText, string buttonName, int userLevel)
+        {
+            Button button = new Button();
+
+            button.Size = new Size(200, 40);
+            button.Location = new Point(10, yCoordinate);
+            yCoordinate += 50;
+
+            button.Text = buttonText;
+            button.Name = buttonName;
+            button.Tag = userLevel;
+
+            return button;
         }
     }
 }
