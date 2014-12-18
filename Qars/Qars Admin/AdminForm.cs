706335213506e8c8d888be8f1f0eea557216a8a0
Niels Boerkamp <niels95b@gmail.com>
2014-12-18 12:12:08 +0100

    - Added event to the buttons - reservationAdminpanel

--- Qars/Qars Admin/AdminForm.cs
+++ Qars/Qars Admin/AdminForm.cs
@@ -1,4 +1,5 @@
 ﻿using Qars;
+using Qars_Admin.Panels;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -25,16 +26,16 @@ namespace Qars_Admin
         private List<Button> categories = new List<Button>();
 
         private CarAdminPanel carAdminPanel;
+        private ReservationAdminPanel reservationAdminPanel;
 
         private System.Drawing.Point adminPanelPosition = new System.Drawing.Point(225, 8);
-
         private int yCoordinate = 50;
 
-        public AdminForm(User currentUser, DBConnect dbConnect) {
+        public AdminForm(User currentUser, DBConnect dbConnect)
+        {
             this.currentUser = currentUser;
             this.databaseConnection = dbConnect;
             addCategories();
-
 
             // make a car adminPanel
             this.carAdminPanel = new CarAdminPanel(this.databaseConnection);
@@ -42,19 +43,27 @@ namespace Qars_Admin
             this.carAdminPanel.Size = new System.Drawing.Size(768, 509);
             this.carAdminPanel.TabIndex = 5;
             this.Controls.Add(this.carAdminPanel);
+
+            //make a reservation adminPanel
+            this.reservationAdminPanel = new ReservationAdminPanel();
+            this.reservationAdminPanel.Location = this.adminPanelPosition;
+            this.reservationAdminPanel.Size = new System.Drawing.Size(768, 509);
+            this.reservationAdminPanel.TabIndex = 6;
+            this.Controls.Add(this.reservationAdminPanel);
+
+
             InitializeComponent();
-
-
         }
-
-        protected override void OnFormClosing(FormClosingEventArgs e) {
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
             //base.OnFormClosing(e);
 
             if (e.CloseReason == CloseReason.WindowsShutDown) return;
 
 
             // Confirm user wants to close
-            switch (MessageBox.Show("Wilt u het programma afsluiten?", "Qars Administratie", MessageBoxButtons.YesNo)) {
+            switch (MessageBox.Show("Wilt u het programma afsluiten?", "Qars Administratie", MessageBoxButtons.YesNo))
+            {
                 case DialogResult.No:
                     e.Cancel = true;
                     break;
@@ -62,7 +71,6 @@ namespace Qars_Admin
                     break;
             }
         }
-
         private void addCategories()
         {
             fillButtonList();
@@ -80,31 +88,57 @@ namespace Qars_Admin
                 }
             }
         }
-
-        private Button createButton(string buttonText, int[] userLevel)
+        private Button createButton(string buttonText, string buttonName, int[] userLevel)
         {
             Button button = new Button();
 
             button.Size = new Size(200, 40);
-
             button.Text = buttonText;
             button.Tag = userLevel;
+            button.Name = buttonName;
+            button.Click += new System.EventHandler(this.categorieButton_Click);
 
             return button;
         }
-
         private void fillButtonList()
         {
-            categories.Add(this.createButton("Autos beheren", new[] {1, 2, 3, 4} ));
-            categories.Add(this.createButton("Reserveringen beheren", new[] { 3, 4 }));
-            categories.Add(this.createButton("Klanten beheren", new[] { 3, 4 }));
-            categories.Add(this.createButton("Algemene Voorwaarden beheren", new[] { 3, 4 }));
-            categories.Add(this.createButton("Schade melden", new[] { 3, 4 }));
-            categories.Add(this.createButton("Forecast weergeven", new[] { 3, 4 }));
+            categories.Add(this.createButton("Autos beheren", "autosBeheren", new[] { 1, 2, 3, 4 }));
+            categories.Add(this.createButton("Reserveringen beheren", "reserveringenBeheren", new[] { 3, 4 }));
+            categories.Add(this.createButton("Klanten beheren", "klantenBeheren", new[] { 3, 4 }));
+            categories.Add(this.createButton("Algemene Voorwaarden beheren", "algemeneVoorwaardenBeheren", new[] { 3, 4 }));
+            categories.Add(this.createButton("Schade melden", "schadeMelden", new[] { 3, 4 }));
+            categories.Add(this.createButton("Forecast weergeven", "forecastWeergeven", new[] { 3, 4 }));
+        }
+        private void AdminForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
         }
 
-        private void AdminForm_FormClosed(object sender, FormClosedEventArgs e) {
-            Application.Exit();
+        private void categorieButton_Click(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            switch (b.Name)
+            {
+                case "autosBeheren":
+                    carAdminPanel.BringToFront();
+                    break;
+                case "reserveringenBeheren":
+                    reservationAdminPanel.BringToFront();
+                    break;
+                case "klantenBeheren":
+                    Console.WriteLine("Nog niet aangemaakt");
+                    break;
+                case "algemeneVoorwaardenBeheren":
+                    Console.WriteLine("Nog niet aangemaakt");
+                    break;
+                case "schadeMelden":
+                    Console.WriteLine("Nog niet aangemaakt");
+                    break;
+                case "forecastWeergeven":
+                    Console.WriteLine("Nog niet aangemaakt");
+                    break;
+            }
+
         }
     }
 }
---
+++ Qars/Qars Admin/Panels/reservationAdminPanel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Qars_Admin.Panels
+{
+    public partial class ReservationAdminPanel : UserControl
+    {
+        public ReservationAdminPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
